f5fcd8bcd13afffc492813becd7ae9aaa2f76890
selincifci <36567499+selincifci@users.noreply.github.com>
2018-03-20 11:07:14 +0300

    Update uygulama02.py

--- uygulama02.py
+++ uygulama02.py
@@ -1,19 +1,17 @@
 "__author__selin çifci"
 #uygulama 2: saniyeyi dakika,saat,gün,ay,yıl şekline dönüştürme.
 
+saniye=36256989896545
 
-number=439849987698764864388758
-result=int(number/60)
-remainder=(number%60)
-print("saat="+str(result))
-print(("dakika="+str(remainder)))
+dakika=int(saniye/60)
+print("dakika="+str(dakika))
 
-number=7330833128312747851776
-result=int(number/30)
-remainder=(number%30)
-print("ay="+str(result))
-print(("gün="+str(remainder)))
+saat=int(dakika/60)
+print("saat="+str(saat))
+gün=int(saat/24)
+print("gün="+str(gün))
+ay=int(gün/30)
+print("ay="+str(ay))
+yıl=int(ay%12)
+print("yıl="+str(yıl))
 
-number=244361104277091581952
-result=int(number/365)
-print("yıl="+str(result))
